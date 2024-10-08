b8328da294fd76bc408c0bd41c36b06928d45647
seanyew25 <seanyew25@gmail.com>
2024-10-08 21:22:04 +0800

    set up firebase

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { onMounted, ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+const auth = getAuth();
+
+function handleClick() {
+  console.log(email.value);
+  console.log(password.value);
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("You're in");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="email" placeholder="Enter your email" />
+    <input v-model="password" placeholder="Enter your password" />
+    <button @click="handleClick()" class="btn btn-primary">Submit</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #da2e2e;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,5 +6,24 @@ import "./styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWSTG1AdhZ0xLTb4l5hbsLol9B-IKOuLk",
+  authDomain: "mathwhiz-f717a.firebaseapp.com",
+  projectId: "mathwhiz-f717a",
+  storageBucket: "mathwhiz-f717a.appspot.com",
+  messagingSenderId: "610807389572",
+  appId: "1:610807389572:web:c7b928a69640b6e6ae91d1",
+  measurementId: "G-JL54QQP5QG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createApp(App).mount("#app");
