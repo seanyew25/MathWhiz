6d93397eb2abf4809a7f925d7697071e2671bbf4
seanyew25 <seanyew25@gmail.com>
2024-10-12 23:22:25 +0800

    Added phaser and environment render

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,21 @@
-<script setup>
+<script>
 import Login from "./components/Login.vue";
-import HomePage from "./components/OldHomePage.vue";
-import Navbar from "./components/Navbar.vue";
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-const router = useRouter();
-const auth = getAuth();
-const isAuthenticated = ref("");
-onMounted(() => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      isAuthenticated.value = true;
-    } else {
-      isAuthenticated.value = false;
-    }
-  });
-});
-
-router.beforeEach(async (to, from) => {
-  // console.log(isAuthenticated.value);
-  if (
-    // make sure the user is authenticated
-    !isAuthenticated.value &&
-    // ❗️ Avoid an infinite redirect
-    to.path !== "/"
-  ) {
-    // redirect the user to the landing page
-    return { path: "/" };
-  }
-});
+import HomePage from "./components/HomePage.vue";
+import { onMounted } from "vue";
+import { initializePhaser } from "./components/Environment.js";
+export default {
+  setup() {
+    onMounted(() => {
+      initializePhaser();
+    });
+  },
+};
 </script>
 
 <template>
-  <Navbar />
-  <RouterView></RouterView>
+  <!--<Login />-->
+  <!-- <HomePage /> -->
+  <div id="phaser-container"></div>
 </template>
 
 <style></style>
