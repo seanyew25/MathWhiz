b0110f2812a6243f14b8e28a1b638d78e0905708
seanyew25 <seanyew25@gmail.com>
2024-10-25 19:56:19 +0800

    - Added tilemap - Added player character - Added walking animations

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
-<script>
+<script setup>
 import Login from "./components/Login.vue";
-import HomePage from "./components/HomePage.vue";
-import { onMounted } from "vue";
-import { initializePhaser } from "./components/Environment.js";
-export default {
-  setup() {
-    onMounted(() => {
-      initializePhaser();
-    });
-  },
-};
+import HomePage from "./components/OldHomePage.vue";
+import Navbar from "./components/Navbar.vue";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const router = useRouter();
+const auth = getAuth();
+const isAuthenticated = ref("");
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isAuthenticated.value = true;
+    } else {
+      isAuthenticated.value = false;
+    }
+  });
+});
+
+router.beforeEach(async (to, from) => {
+  // console.log(isAuthenticated.value);
+  if (
+    // make sure the user is authenticated
+    !isAuthenticated.value &&
+    // ❗️ Avoid an infinite redirect
+    to.path !== "/"
+  ) {
+    // redirect the user to the landing page
+    return { path: "/" };
+  }
+});
 </script>
 
 <template>
-  <!--<Login />-->
-  <!-- <HomePage /> -->
-  <div id="phaser-container"></div>
+  <Navbar />
+  <RouterView></RouterView>
 </template>
 
 <style></style>
