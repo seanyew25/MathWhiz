a5d5adc948c5c1599dd7bc9c4b176b42a5deba06
seanyew25 <seanyew25@gmail.com>
2024-10-27 11:53:30 +0800

    Added page routing to quiz game pages upon door collision with target locations - Added event listener in main vue file to listen to door collisions - Added integration with vue router such that page is routed to the corresponding quiz game page upon door collision - Added functionality to destroy game beforeRouteLeave to prevent WebGl errors with assets

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,11 +4,88 @@
 <script>
 import { onMounted } from "vue";
 import { initializePhaser } from "./Environment.js";
+import { useRouter } from "vue-router";
 export default {
-  setup() {
-    onMounted(() => {
-      initializePhaser();
+  mounted() {
+    this.game = initializePhaser();
+    this.game.scene.start("MainScene");
+    console.log(this.game.scene);
+    this.getSceneStatus().then(() => {
+      console.log(`scene: ${this.game.scene.getScene("MainScene")}`);
+      this.handleDoorCollision();
     });
+    this.router = useRouter();
+  },
+  data() {
+    return {
+      game: null,
+      router: null,
+    };
+  },
+  //DESTROY GAME BEFORE ROUTE LEAVE ELSE WEBGL ERROR WILL APPEAR
+  beforeRouteLeave() {
+    if (this.game && this.game.destroy) {
+      this.game.destroy(true); // destroys all phaser dom elements
+    }
+  },
+  methods: {
+    getSceneStatus() {
+      let game = this.game;
+      function getSceneStatus(gameObj) {
+        // console.log(gameObj.scene.getScene("MainScene"));
+        if (gameObj.scene.getScene("MainScene")) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      return new Promise((resolve, reject) => {
+        // let status = false;
+        // let count = 0;
+        // status = checkSceneStatus(game);
+        function checkStatus(game) {
+          if (getSceneStatus(game)) {
+            resolve("scene found");
+          } else {
+            // console.log("Scene not found!");
+            setTimeout(() => {
+              checkStatus(game);
+            }, 500);
+          }
+        }
+        checkStatus(game);
+      });
+    },
+    handleDoorCollision() {
+      // console.log(`from collision: ${this.game}`);
+      let scene = this.game.scene.getScene("MainScene");
+      console.log(`main scene: ${scene}`);
+
+      if (!scene) {
+        console.error("Scene not found!");
+        return;
+      }
+      console.log(scene);
+
+      scene.events.on("doorCollision", (doorObj) => {
+        console.log(`door: ${doorObj.name}`);
+        const name = doorObj.name;
+        if (name === "homeDoor") {
+          this.router.push("/profile");
+        } else if (name === "bankDoor") {
+          this.router.push("/bank");
+        } else if (
+          name === "shoppingCentreDoor1" ||
+          name === "shoppingCentreDoor2"
+        ) {
+          this.router.push("/shoppingCentre");
+        } else if (name === "schoolDoor1" || name === "schoolDoor2") {
+          this.router.push("/school");
+        } else if (name === "bakeryDoor") {
+          this.router.push("/bakery");
+        }
+      });
+    },
   },
 };
 </script>
@@ -16,6 +93,5 @@ export default {
 #phaser-container {
   overflow: hidden;
   height: calc(100vh - 56px);
-  /* flex: 1; */
 }
 </style>
---
+++ src/components/ProfilePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="tw-text-blue-500">Profile Page</h1>
+  </div>
+</template>
+<style scoped></style>
+<script setup></script>
