fe188bf4c0f4afd1d0a1dae66f9591085a3b8cca
seanyew25 <seanyew25@gmail.com>
2024-10-27 13:43:42 +0800

    Testing for deployment - Moved image assets to public folder - Changed paths to assets to reflect new file locations

--- src/components/ShoppingCentrePage.vue
+++ src/components/ShoppingCentrePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Shopping Centre</h1>
-    <h1 class="tw-text-blue-500 tw-decoration-solid">
+    <h1 class="tw-text-blue-500 tw-decoration-solid tw-underline">
       if this is blue and underlined, Tailwind is working :D
     </h1>
   </div>
