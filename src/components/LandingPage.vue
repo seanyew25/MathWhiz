ff7ce6e4679e362e893fbf288f82a68b9ce5ffac
setsunaxe7 <eliseteo0717@gmail.com>
2024-10-31 01:50:10 +0800

    Update LandingPage.vue

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,5 +1,35 @@
 <template>
   <h1 class="tw-bg-blue-500 tw-decoration-dashed tw-underline">Landing Page</h1>
+  <div class="background-container">
+    <h1 class="hero-title">
+      Welcome to Mathwhiz!
+    </h1>
+    <h3>We Gamify Primary School Math</h3>
+  </div>
+  <div class="bento-ui">
+    
+  </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&display=swap");
+
+.hero-title{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.background-container{
+  background-image: linear-gradient(to right, #B7E0FF, #95e3ff);
+  /* background-color: #B7E0FF; */
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
 
 <script setup></script>
