1e1deea6b001285df993577b6d5e5912a29aac34
seanyew25 <seanyew25@gmail.com>
2024-11-03 16:00:36 +0800

    Added animation where card flip horizontally to show cat stats

--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="tw-h-full tw-p-6 tw-gap-4 tw-bg-[#B7E0FF]">
-    <div class="tw-grid tw-grid-cols-3 tw-w-[80%] tw-margin-auto">
+  <div class="tw-h-full tw-p-6 tw-bg-[#B7E0FF]">
+    <div
+      class="tw-grid tw-gap-x-2 tw-gap-y-4 md:tw-gap-y-8 tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-3 xl:tw-grid-cols-4 tw-w-full md:tw-w-[80%] tw-mx-auto"
+    >
       <CardComponent
-        imgLocation="/assets/mainassets/morgana.png"
-        furName="Morgana"
-        furDesc="The Original. 
-        This cat will steal your heart."
-      />
-      <CardComponent
-        imgLocation="/assets/mainassets/morgana.png"
-        furName="Calico"
-        furDesc="A patchwork of warmth and mischief"
-      />
-      <CardComponent
-        imgLocation="/assets/mainassets/morgana.png"
-        furName="Midnight"
-        furDesc="Sleek and shadowy"
+        class="tw-justify-self-center"
+        v-for="cat in cats"
+        :key="cat.name"
+        :catObj="cat"
       />
     </div>
   </div>
@@ -25,6 +17,42 @@ import CardComponent from "../components/shop/CardComponent.vue";
 export default {
   components: {
     CardComponent,
+  },
+  data() {
+    return {
+      cats: [
+        {
+          name: "Morgana",
+          cuteness: 92,
+          fluffiness: 85,
+          coolness: 90,
+          color: "Siamese",
+          imgLocation: "/assets/mainassets/morgana.png",
+          description: "The Original. This cat will steal your heart.",
+          rarity: "Rare",
+        },
+        {
+          name: "Calico",
+          cuteness: 88,
+          fluffiness: 75,
+          coolness: 78,
+          color: "Calico",
+          imgLocation: "/assets/mainassets/morgana.png",
+          description: "Cute, cuddly and colourful",
+          rarity: "Common",
+        },
+        {
+          name: "Midnight",
+          cuteness: 80,
+          fluffiness: 90,
+          coolness: 95,
+          color: "Black",
+          imgLocation: "/assets/mainassets/morgana.png",
+          description: "Dark and sleek",
+          rarity: "Ultra Rare",
+        },
+      ],
+    };
   },
 };
 </script>
