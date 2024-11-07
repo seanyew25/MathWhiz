170e14fd260f98947e09e247ca84264de97a0af2
Eugene TAN <eugene.tan.2023@scis.smu.edu.sg>
2024-11-08 01:51:52 +0800

    Update SchoolPage

--- src/pages/SchoolPage.vue
+++ src/pages/SchoolPage.vue
@@ -1,16 +1,47 @@
 <template>
   <div>
-    <SchoolASPage/>
-    <SchoolMDPage/>
+    <SchoolCutscene
+      v-if="showCutscene"
+      @end-cutscene="endCutscene"
+      @go-to-addition-subtraction="goToAdditionSubtraction"
+      @go-to-multiplication-division="goToMultiplicationDivision"
+    />
+    <AdditionSubtractGame v-else-if="showAdditionSubtraction" />
+    <MuliplicationDivisionGame v-else-if="showMultiplicationDivision" />
   </div>
 </template>
-<style scoped></style>
-<script setup>
-// import SchoolASPage from "ASComponent.vue";
-// import SchoolMDPage from "MDComponent.vue";
-import GameComponent from "./GameComponent.vue";
 
-//rename GameComponent.vue to MDComponent.vue
-// my vue template will be under ASComponent.vue
-// your vue template will be under MDComponent.vue
+<script setup>
+import MuliplicationDivisionGame from "../components/school/MultiplicationDivisionGame.vue";
+import AdditionSubtractGame from "../components/school/AdditionSubtractGame.vue";
+import { ref, onMounted } from "vue";
+import SchoolCutscene from "../components/school/SchoolCutscene.vue";
+
+const showCutscene = ref(true); // Show cutscene initially
+const showAdditionSubtraction = ref(false);
+const showMultiplicationDivision = ref(false);
+
+// Function to display MarketCounting after the cutscene
+function goToAdditionSubtraction() {
+  showAdditionSubtraction.value = true;
+  showCutscene.value = false;
+}
+
+// Function to display MarketOrdering after the cutscene
+function goToMultiplicationDivision() {
+  showMultiplicationDivision.value = true;
+  showCutscene.value = false;
+}
+
+// Handler for when the cutscene ends
+function endCutscene() {
+  showCutscene.value = false; // Hide cutscene
+}
+
+// Reset state when the component is mounted
+onMounted(() => {
+  showCutscene.value = true;
+  showAdditionSubtraction.value = false;
+  showMultiplicationDivision.value = false;
+});
 </script>
